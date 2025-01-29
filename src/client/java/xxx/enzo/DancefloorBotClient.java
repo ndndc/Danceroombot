62d43c0abc6b3236c8a578a5ddc8bb1889526dd3
im23b-terenzianie <terenzianie@bzz.ch>
2025-01-29 13:18:42 +0100

    first commit

---
+++ src/client/java/xxx/enzo/DancefloorBotClient.java
@@ -0,0 +1,10 @@
+package xxx.enzo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DancefloorBotClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
